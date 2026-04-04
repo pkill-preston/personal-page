87a4a25b6f6bd319e06394848b32ac8e7d4f189c
pkill-preston <0v3rthinker@protonmail.com>
2026-04-04 14:24:21 -0300

    adding new i18n parameter

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,5 +3,6 @@ import type {LocalePrefix} from "next-intl/routing";
 export const routing = {
 	locales: ["en", "es", "pt"] as const,
 	defaultLocale: "en",
-	localePrefix: "always" satisfies LocalePrefix 
+	localeDetection: true,
+	localePrefix: "always" satisfies LocalePrefix
 } as const;
